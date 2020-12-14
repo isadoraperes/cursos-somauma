fd6911adb77ed808fe9dc0bbe92144fe071aecb9
Isadora Peres <isadoraoperes@gmail.com>
2020-12-14 15:48:35 -0300

    add model curso

--- cursos/src/main/java/br/com/somauma/model/Curso.java
+++ cursos/src/main/java/br/com/somauma/model/Curso.java
@@ -1,5 +1,79 @@
 package br.com.somauma.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "tb_curso")
 public class Curso {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long id;
+	
+	@NotNull
+	@Size(min = 1, max = 50)
+	private String nome;
+	
+	@Size(min = 1, max = 150)
+	private String descricao;
+	
+	@Size(min = 1, max = 50)
+	private String nivel;
+	
+	@NotNull
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getNivel() {
+		return nivel;
+	}
+
+	public void setNivel(String nivel) {
+		this.nivel = nivel;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
 }
